479f4b4475ae2ca6b80caa019434b465852fcf1b
Ayush tomar <ayushtomar388@gmail.com>
2025-01-01 00:02:56 +0530

    Lab layout created

---
+++ src/app/lab/layout.tsx
@@ -0,0 +1,9 @@
+const LabLayout = ({ children }: { children: React.ReactNode }) => {
+    return ( 
+        <div className="min-h-screen place-content-center">
+            {children}
+        </div>
+     );
+}
+ 
+export default LabLayout;
