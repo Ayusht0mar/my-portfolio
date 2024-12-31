9222af261623299aa689fb507b9241d72b930843
Ayush tomar <ayushtomar388@gmail.com>
2025-01-01 00:00:46 +0530

    Styled

--- src/components/sections/TechButton.tsx
+++ src/components/sections/TechButton.tsx
@@ -12,7 +12,7 @@ const TechButton = ({
     return ( 
         <div className="group flex flex-col items-center border border-[#242424] py-1.5 px-3 rounded w-fit">
             <h4 className="text-[15px]">{label}</h4>
-            <div className="bg-[#1C1C1C] border border-[#242424] mt-8 rounded-md overflow-hidden hidden group-hover:block absolute">
+            <div className="bg-[#1C1C1C] border border-[#242424] min-h-20 min-w-20 mt-8 rounded-md overflow-hidden hidden group-hover:block absolute">
                 <Image src={imgSrc} alt={label} width={80} height={80} className="p-1.5 hiddenm"/>
 
             </div>
