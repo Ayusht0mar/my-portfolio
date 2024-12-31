841719e3d7232f6ff3054156af4676af7dcb0266
Ayush tomar <ayushtomar388@gmail.com>
2024-12-31 23:56:11 +0530

    SocialSection completed

--- src/components/sections/SocialSection.tsx
+++ src/components/sections/SocialSection.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { ArrowUpRight } from "lucide-react";
 
 const SocialSection = () => {
     return ( 
@@ -8,37 +7,37 @@ const SocialSection = () => {
             <div className="grid grid-cols-3 gap-4">
                 <div className="aspect-square border border-[#242424] rounded-xl p-6 flex flex-col justify-between">
                     <div className="flex flex-col gap-4">
-                        <Image src="/tech/react.svg" alt="Inconcise" width={32} height={32}/>
+                        <Image src="/tech/github.svg" alt="GitHub" width={48} height={48}/>
                         <div>
                             <h3 className="text-[#9B9B9B] font-bold ">Ayush Tomar</h3>
-                            <p className="text-sm">@ayushtomar</p>
+                            <p className="text-sm">@ayusht0mar</p>
                         </div>
                     </div>
-                    <a href="/" className="bg-black w-fit py-1.5 px-[18px] rounded-full">
+                    <a href="https://github.com/Ayusht0mar" target="_blank" className="bg-black text-white w-fit py-1.5 px-[18px] rounded-full">
                         <p className="font-semibold">Follow</p>
                     </a>
                 </div>
                 <div className="aspect-square border border-[#242424] rounded-xl p-6 flex flex-col justify-between">
                     <div className="flex flex-col gap-4">
-                        <Image src="/tech/react.svg" alt="Inconcise" width={32} height={32}/>
+                        <Image src="/tech/linkedin.svg" alt="Inconcise" width={48} height={48}/>
                         <div>
                             <h3 className="text-[#9B9B9B] font-bold ">Ayush Tomar</h3>
-                            <p className="text-sm">@ayushtomar</p>
+                            <p className="text-sm">@ayusht0mar</p>
                         </div>
                     </div>
-                    <a href="/" className="bg-black w-fit py-1.5 px-[18px] rounded-full">
+                    <a href="https://www.linkedin.com/in/ayusht0mar/" className="bg-[#0077B5] text-white w-fit py-1.5 px-[18px] rounded-full">
                         <p className="font-semibold">Connect</p>
                     </a>
                 </div>
                 <div className="aspect-square border border-[#242424] rounded-xl p-6 flex flex-col justify-between">
                     <div className="flex flex-col gap-4">
-                        <Image src="/tech/react.svg" alt="Inconcise" width={32} height={32}/>
+                        <Image src="/tech/x.svg" alt="Inconcise" width={48} height={48}/>
                         <div>
                             <h3 className="text-[#9B9B9B] font-bold ">Ayush Tomar</h3>
-                            <p className="text-sm">@ayushtomar</p>
+                            <p className="text-sm">@justayushtomar</p>
                         </div>
                     </div>
-                    <a href="/" className="bg-black w-fit py-1.5 px-[18px] rounded-full">
+                    <a href="https://x.com/justayushtomar" className="bg-black text-white w-fit py-1.5 px-[18px] rounded-full">
                         <p className="font-semibold">Follow</p>
                     </a>
                 </div>
