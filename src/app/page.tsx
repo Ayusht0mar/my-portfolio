bfb078fbc078092350a7088c5db5dc8cbb6cc784
Ayush tomar <ayushtomar388@gmail.com>
2024-12-31 23:57:49 +0530

    Styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ import VenturesSection from "@/components/sections/VenturesSection";
 
 export default function Home() {
   return (
-    <div className="mx-auto mt-16 w-[80vw] max-w-7xl grid gap-20 grid-cols-3">
+    <div className="mx-auto mt-16 w-[80vw] max-w-7xl grid gap-10 grid-cols-3">
         <div>
           <AboutSection/>
         </div>
-        <div className="flex flex-col gap-[60px] col-span-2 mt-4">
+        <div className="flex flex-col gap-[60px] col-span-2 mt-2 pb-20">
           <ProjectsSection/>
           <TechStackSection/>
           <VenturesSection/>
