76dfb611c93d5fc45fded4e3e055b2031c476381
Ayush tomar <ayushtomar388@gmail.com>
2025-01-06 23:44:39 +0530

    Not found page created

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <div className='flex flex-col gap-2 min-h-screen justify-center items-center'>
+      <h2 className='text-neutral-200 text-3xl'>Opps! The page doesn't exist</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  )
+}
