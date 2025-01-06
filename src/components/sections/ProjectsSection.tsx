c6f2657c98a9b825925408bf596b75d5129f72ce
Ayush tomar <ayushtomar388@gmail.com>
2025-01-06 23:54:45 +0530

    ProjectsSection added and View more button added

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -1,8 +1,13 @@
+import ProjectCardGrid from "../ProjectCardsGrid";
+
 const ProjectsSection = () => {
     return ( 
         <div className="flex flex-col gap-4">
-            <h2 className="text-xl font-bold text-white">Projects</h2>
-            Projects Section
+            <div className="flex items-baseline justify-between">
+                <h2 className="text-xl font-bold text-white">Projects</h2>
+                <a href="/projects" className="hover:text-white">view more</a>
+            </div>
+            <ProjectCardGrid/>
         </div>
      );
 }
