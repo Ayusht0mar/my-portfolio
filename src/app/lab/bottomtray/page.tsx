dd712e5077193e6991ff3e92b6830458dbd91454
Ayush tomar <ayushtomar388@gmail.com>
2025-01-06 23:48:58 +0530

    Bottom Tray page created

---
+++ src/app/lab/bottomtray/page.tsx
@@ -0,0 +1,13 @@
+import BottomTray from "@/components/lab/BottomTray";
+import LabFloatingButton from "@/components/lab/LabFloatingButton";
+
+const BottomTrayPage = () => {
+    return ( 
+        <div>
+            <LabFloatingButton sourceLink="https://github.com/Ayusht0mar/my-portfolio/blob/master/src/components/lab/BottomTray.tsx"/>
+            <BottomTray/>
+        </div>
+     );
+}
+ 
+export default BottomTrayPage;
