b4fe46cb5f8d28f4ed2e77607b6573be681ad964
Ayush tomar <ayushtomar388@gmail.com>
2025-01-24 16:21:05 +0530

    Add : lab floating button

--- src/app/lab/kanbanboard/page.tsx
+++ src/app/lab/kanbanboard/page.tsx
@@ -1,8 +1,12 @@
 import { KanbanBoard } from "@/components/lab/KanbanBoard";
+import LabFloatingButton from "@/components/lab/LabFloatingButton";
 
 const KanbanBoardPage = () => {
     return ( 
-        <KanbanBoard/>
+        <div>
+            <LabFloatingButton sourceLink="https://github.com/Ayusht0mar/my-portfolio/blob/master/src/components/lab/KanbanBoard.tsx"/> 
+            <KanbanBoard/>
+        </div>
      );
 }
  
--- src/app/lab/logomarquee/page.tsx
+++ src/app/lab/logomarquee/page.tsx
@@ -1,8 +1,10 @@
+import LabFloatingButton from "@/components/lab/LabFloatingButton";
 import LogoMarquee from "@/components/lab/LogoMarquee";
 
 const logoMarqueePage = () => {
     return ( 
         <div>
+            <LabFloatingButton sourceLink="https://github.com/Ayusht0mar/my-portfolio/blob/master/src/components/lab/LogoMarquee.tsx"/> 
             <LogoMarquee/>
         </div>
      );
