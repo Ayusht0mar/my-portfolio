7b9e53bfb26d57c2ff4d27c2458bc0072a1b4939
Ayush tomar <ayushtomar388@gmail.com>
2025-01-24 16:17:44 +0530

    Update: projects data

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,13 @@
 import { Github, Globe, Home } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 const projects = [
     {
         label: "Airbnb Clone",
         imgSrc: "/project/airbnb-cover.png",
-        githublink: "/",
-        livelink: "/"
+        githublink: "https://github.com/Ayusht0mar/airbnb-clone",
+        livelink: "https://airbnb-clone-ayush.vercel.app/"
     },
     {
         label: "Miro Clone",
@@ -17,57 +18,60 @@ const projects = [
     {
         label: "Discord Clone",
         imgSrc: "/project/discord-cover.png",
-        githublink: "/",
-        livelink: "/"
+        githublink: "https://github.com/Ayusht0mar/discord-clone",
+        livelink: "https://discord-clone-ayush.vercel.app/"
     },
     {
         label: "Clarity",
-        imgSrc: "/project/airbnb-cover.png",
-        githublink: "/",
-        livelink: "/"
+        imgSrc: "/project/clarity-cover.png",
+        githublink: "https://github.com/Ayusht0mar/clarity",
+        livelink: "https://forclarity.vercel.app/"
     },
     {
-        label: "Linktree",
-        imgSrc: "/project/airbnb-cover.png",
-        githublink: "/",
-        livelink: "/"
+        label: "Speaksy",
+        imgSrc: "/project/speaksy-cover.png",
+        githublink: "https://github.com/Ayusht0mar/speaksy",
+        livelink: ""
     },
     {
-        label: "Airbnb Clone",
-        imgSrc: "/project/airbnb-cover.png",
-        githublink: "/",
-        livelink: "/"
+        label: "Flux",
+        imgSrc: "/project/flux-cover.png",
+        githublink: "https://github.com/Ayusht0mar/flux",
+        livelink: "https://flux-inconcise.vercel.app/"
     },
-    {
-        label: "Airbnb Clone",
-        imgSrc: "/project/airbnb-cover.png",
-        githublink: "/",
-        livelink: "/"
-    },
-    {
-        label: "Airbnb Clone",
-        imgSrc: "/project/airbnb-cover.png",
-        githublink: "/",
-        livelink: "/"
-    },
+    // {
+    //     label: "Linktree",
+    //     imgSrc: "/project/airbnb-cover.png",
+    //     githublink: "/",
+    //     livelink: "/"
+    // },
+    // {
+    //     label: "Hello",
+    //     imgSrc: "/project/airbnb-cover.png",
+    //     githublink: "/",
+    //     livelink: "/"
+    // },
 ]
 
 const ProjectsPage = () => {
     return ( 
         <div className="w-[90vw] mx-auto max-w-xl mt-5 md:mt-10 space-y-4">
             <div className="flex text-xl items-center gap-2 pl-2">
-                <a href="/"><Home size={20}/></a>/ <p className="text-base">Projects</p>
+                <Link href="/"><Home size={20}/></Link>/ <p className="text-base">Projects</p>
             </div>
             <div className="grid  md:grid-cols-2 gap-5 pb-10">
                 {projects.map(({ label, imgSrc, githublink, livelink }) => (
-                        <div className= "rounded-xl border border-[#242424] p-2 space-y-2">
+                        <div key={label} className= "rounded-xl border border-[#242424] p-2 space-y-2">
                             <Image src={imgSrc} alt={label} width={9999} height={9999} className="rounded-lg" />
                             <div className="flex justify-between items-center px-1">
                                 <h2>{label}</h2>
                                 <div className="flex gap-2">
-                                    <a href={livelink}>
-                                       <Globe size={18}/>
-                                    </a>
+
+                                    {livelink &&
+                                        <a href={livelink}>
+                                        <Globe size={18}/>
+                                        </a>
+                                    }
                                     <a href={githublink}>
                                         <Github size={18}/>
                                     </a>
