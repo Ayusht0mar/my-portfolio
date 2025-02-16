84bbf5a271f4acbe96b2315f6c4b62f9233c76c6
Ayush tomar <ayushtomar388@gmail.com>
2025-02-16 14:19:12 +0530

    add: Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google'
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +23,7 @@ export default function RootLayout({
         className={inter.className}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
